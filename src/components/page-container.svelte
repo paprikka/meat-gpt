16093b8c08c6809a6db696e5bde778ab701f6361
Rafal Pastuszak <rafal.pastuszak@gmail.com>
2023-05-19 16:40:08 +0100

    wip:

---
+++ src/components/page-container.svelte
@@ -0,0 +1,18 @@
+<div class="page-container">
+	<slot />
+</div>
+
+<style>
+	.page-container {
+		width: 100vw;
+		height: 100vh;
+		padding: var(--page-margin);
+	}
+
+	@media all and (min-width: 768px) {
+		.page-container {
+			max-width: 30rem;
+			margin: 0 auto;
+		}
+	}
+</style>
