272b8b10f6daf3d1e168bb2a65962ad34a2bdec9
Rafal Pastuszak <rafal.pastuszak@gmail.com>
2023-05-23 12:20:29 +0100

    feat: cock and done

--- src/routes/done/+page.svelte
+++ src/routes/done/+page.svelte
@@ -5,13 +5,19 @@
 </script>
 
 <PageContainer>
-	<main><p><button on:click={sendEmail}>❤️ click me</button></p></main>
-	<Footer>
-		<nav>
-			<a href="https://sonnet.io" target="_blank">About</a>
-		</nav>
-	</Footer>
+	<main>
+		<p>
+			<button on:click={sendEmail}><strong>❤️</strong> click me</button>
+		</p>
+	</main>
 </PageContainer>
+<Footer>
+	<nav>
+		<a class="me" href="https://sonnet.io" target="_blank">Made with 🐐 and Rafal</a>
+
+		<span>Original music by <a href="https://itch.io/profile/dos88" target="_blank">DOS88</a></span>
+	</nav>
+</Footer>
 
 <style>
 	main {
@@ -34,6 +40,53 @@
 		font-size: 2rem;
 		background: none;
 		border: none;
+		padding: 0.5rem 0.75rem;
 		cursor: pointer;
+		box-shadow: 0 0 0 0.3rem;
+		border-radius: 0.5rem;
+	}
+
+	button strong {
+		display: inline-block;
+		transition: 0.3s scale, 0.3s rotate;
+		text-shadow: 0 0 0.75rem red;
+	}
+
+	@media (hover: hover) {
+		button:hover {
+			background-color: var(--color-text);
+			color: var(--color-bg);
+			box-shadow: 0 0 0 0.3rem var(--color-text);
+		}
+
+		button:hover strong {
+			scale: 2.5;
+		}
+	}
+
+	button:active {
+		background-color: var(--color-text);
+		color: var(--color-bg);
+		box-shadow: 0 0 0 0.3rem var(--color-text);
+	}
+
+	button:active strong {
+		scale: 2.5;
+		rotate: -20deg;
+	}
+
+	nav {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+
+	nav a {
+		font-size: 1rem;
+	}
+
+	nav a.me {
+		font-size: 1.25rem;
 	}
 </style>
